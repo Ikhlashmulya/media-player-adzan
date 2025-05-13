2e458627bc64fd09d99b4ff8af663ee9a569e320
ikhlashmulya <nurahmanmulya@gmail.com>
2025-05-13 14:17:54 +0700

    add asset

--- data/adzan-list.ts
+++ data/adzan-list.ts
@@ -7,4 +7,10 @@ export const adzanList: Adzan[] = [
     title: "Adzan Mekkah",
     sound: require("@/assets/audio/Adzan-Mekkah_Abdul-Aziz-Zahrani.mp3"),
   },
+  {
+    flag: require("@/assets/images/Flag_of_Indonesia.png"),
+    muadzin: "Syamsuri Firdaus",
+    title: "Adzan Indonesia",
+    sound: require("@/assets/audio/Adzan_Syamsuri_Firdaus.mp3"),
+  },
 ];
